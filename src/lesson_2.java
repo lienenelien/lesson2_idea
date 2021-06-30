737ae9fd814bfd88f51262dc8e7b371e09313c4f
lienenelien <liene.lagzdina25@gmail.com>
2021-06-30 21:17:46 +0300

    initial commit

---
+++ src/lesson_2.java
@@ -0,0 +1,8 @@
+public class lesson_2 {
+
+    public static void main(String[] args) {
+        System.out.println("hello, ,teacher");
+        System.out.println("it is a lovely day outside today!");
+
+    }
+}
